862028e3f8b11b07d32d70fe72f9380a4f2bcf3f
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-12-17 16:34:34 +0100

    Début du filtre des annonces par villes

--- simplon/src/main/java/com/project/simplon/repository/AnnonceRepository.java
+++ simplon/src/main/java/com/project/simplon/repository/AnnonceRepository.java
@@ -1,10 +1,24 @@
 package com.project.simplon.repository;
 import com.project.simplon.model.Annonce;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface AnnonceRepository.
+ */
 @Repository
 public interface AnnonceRepository extends JpaRepository<Annonce, Long> {
+	
+	/**
+	 * Find by city.
+	 *
+	 * @param city the city
+	 * @return the list
+	 */
+	List<Annonce> findByCity(String city);
 
 }
